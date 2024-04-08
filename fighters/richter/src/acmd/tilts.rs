3062c25c59d37a94548df136706908ce82d31c0a
just.rob <ryursa@gmail.com>
2024-04-08 08:19:54 -0400

    dtilt2

--- fighters/richter/src/acmd/tilts.rs
+++ fighters/richter/src/acmd/tilts.rs
@@ -323,18 +323,11 @@ unsafe extern "C" fn expression_attacklw3(agent: &mut L2CAgentBase) {
 unsafe extern "C" fn game_attacklw32(agent: &mut L2CAgentBase) {
     let lua_state = agent.lua_state_agent;
     let boma = agent.boma();
-    frame(lua_state, 1.0);
-    FT_MOTION_RATE_RANGE(agent, 1.0, 5.0, 6.0);
-    frame(lua_state, 5.0);
-    FT_MOTION_RATE(agent, 1.0);
     frame(lua_state, 6.0);
     if is_excute(agent) {
         JostleModule::set_status(boma, false);
         WorkModule::on_flag(boma, *FIGHTER_SIMON_STATUS_ATTACK_LW32_WORK_ID_FLAG_LANDING_AIR);
     }
-    frame(lua_state, 7.0);
-    sv_kinetic_energy!(set_speed_mul, agent, FIGHTER_KINETIC_ENERGY_ID_MOTION, 0.8);
-    FT_MOTION_RATE_RANGE(agent, 7.0, 34.0, 18.0);
     frame(lua_state, 8.0);
     if is_excute(agent) {
         ATTACK(agent, 0, 0, Hash40::new("kneel"), 5.5, 72, 60, 0, 88, 3.5, 0.0, 1.0, -1.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
@@ -350,13 +343,11 @@ unsafe extern "C" fn game_attacklw32(agent: &mut L2CAgentBase) {
         ATTACK(agent, 0, 0, Hash40::new("kneel"), 3.5, 72, 60, 0, 88, 3.5, 0.0, 1.0, -1.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
         ATTACK(agent, 1, 0, Hash40::new("footl"), 3.5, 72, 60, 0, 88, 3.0, 0.0, 1.5, -2.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_KICK);
     }
-    frame(lua_state, 26.0);
+    frame(lua_state, 24.0);
     if is_excute(agent) {
         AttackModule::clear_all(boma);
         JostleModule::set_status(boma, true);
     }
-    frame(lua_state, 34.0);
-    FT_MOTION_RATE(agent, 1.0);
 }
 
 pub fn install(agent: &mut Agent) {
--- fighters/richter/src/status/mod.rs
+++ fighters/richter/src/status/mod.rs
@@ -1,11 +1,13 @@
 use super::*;
 use globals::*;
 
+mod attacklw3;
 mod attacks3;
 mod special_n;
 mod special_s;
 
 pub fn install(agent: &mut Agent) {
+    attacklw3::install(agent);
     attacks3::install(agent);
     special_n::install(agent);
     special_s::install(agent);
