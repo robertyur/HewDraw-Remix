983991ef1f49caceea0af7f83d52aa2f51a92505
jobrien97 <47401664+jobrien97@users.noreply.github.com>
2024-11-22 21:01:08 -0500

    Revert "Wall Jump Rewrite"

--- fighters/common/src/general_statuses/mod.rs
+++ fighters/common/src/general_statuses/mod.rs
@@ -270,8 +270,7 @@ unsafe fn sub_transition_group_check_air_wall_jump(fighter: &mut L2CFighterCommo
     // basic validity checks
     if fighter.global_table[SITUATION_KIND].get_i32() != *SITUATION_KIND_AIR
     || fighter.is_status(*FIGHTER_STATUS_KIND_WALL_JUMP)
-    || fighter.get_int(*FIGHTER_INSTANCE_WORK_ID_INT_WALL_JUMP_COUNT) >= ParamModule::get_int(fighter.battle_object, ParamType::Common, "wall_jump_count_airtime")
-    || fighter.get_int(*FIGHTER_INSTANCE_WORK_ID_INT_DISABLE_WALL_JUMP_FRAME) > 0 {
+    || fighter.get_int(*FIGHTER_INSTANCE_WORK_ID_INT_WALL_JUMP_COUNT) >= 2 {
         return false.into();
     }
 
